f2b6d3be211f9182a1326dcf1c9f46dacbc8c343
lily-bonestech <53164491+lily-bonestech@users.noreply.github.com>
2019-12-13 11:38:32 +0800

    Add files via upload

---
+++ clipping-with-caps/js/uniforms.js
@@ -0,0 +1,49 @@
+CAPS.UNIFORMS = {
+
+	clipping: {
+		//color:        { type: "c",  value: new THREE.Color( 0x3d9ecb ) },
+		color:        { type: "c",  value: new THREE.Color( 0xcbf1f9) },
+		clippingLow:  { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) },
+		clippingHigh: { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) }
+	},
+
+
+	clipping3: {
+		//color:        { type: "c",  value: new THREE.Color( 0x3d9ecb ) },
+		color:        { type: "c",  value: new THREE.Color( 0xff3232 ) },
+		clippingLow:  { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) },
+		clippingHigh: { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) }
+	},
+
+	caps: {
+		color: { type: "c", value: new THREE.Color( 0x00ffff ) }
+		//color: { type: "c", value: new THREE.Color( 0xf83610 ) }
+		//color: { type: "c", value: new THREE.Color( 0x86d8ce )}
+
+	}
+	
+};
+
+
+CAPS.UNIFORMS3 = {
+
+	
+	clipping: {
+		//color:        { type: "c",  value: new THREE.Color( 0x3d9ecb ) },
+		color:        { type: "c",  value: new THREE.Color( 0xff005a) },
+		clippingLow:  { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) },
+		clippingHigh: { type: "v3", value: new THREE.Vector3( 0, 0, 0 ) }
+	},
+
+	caps: {
+		color: { type: "c", value: new THREE.Color( 0xf64572 ) }
+		//color: { type: "c", value: new THREE.Color( 0xf83610 ) }
+		//color: { type: "c", value: new THREE.Color( 0x86d8ce )}
+
+	}
+
+};
+
+
+
+
